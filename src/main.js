1eb651b1b64a52eecca7e73343dde98721c3dfd6
Daya Kumaran <dayakumaran2012@gmail.com>
2025-11-13 10:35:01 -0800

    added flag

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { createGround } from './objects/Ground.js';
 import { createWall } from './objects/Walls.js';
 import { createCamera, setupFlyCamera } from './core/Camera.js';
+import {createFlag} from './objects/Flag.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87ceeb); 
@@ -26,6 +27,12 @@ scene.add(ground);
 const walls = createWall();
 walls.forEach(wall => scene.add(wall));
 
+createFlag('/models/golf_flag.glb', {
+  position: new THREE.Vector3(30,8,30),
+  scale: new THREE.Vector3(5,5,5),
+}).then(model => {
+  scene.add(model)
+});
 
 camera.position.set(0, 2, 10); 
 
