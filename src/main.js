3cdcbe7f646535e2a7d25ba295869aab962f0ae3
euniceyahn <euniceyooahn@gmail.com>
2025-11-13 18:58:32 -0800

    Added cube obstacles

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createGround } from './objects/Ground.js';
 import { createWall } from './objects/Walls.js';
 import { createCamera, setupFlyCamera } from './core/Camera.js';
 import {createFlag} from './objects/Flag.js';
+import { createCubeObstacle } from './objects/CubeObstacle.js';
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0x87ceeb); 
@@ -26,6 +27,16 @@ scene.add(ground);
 
 const walls = createWall();
 walls.forEach(wall => scene.add(wall));
+
+// Adding individual cube obstacles
+const cube1 = createCubeObstacle({ x: 10, y: 1, z: 20 }, { x: 6, y: 2, z: 6 }); 
+scene.add(cube1);
+
+const cube2 = createCubeObstacle({ x: -10, y: 1, z: 10 }, { x: 8, y: 2, z: 8 }); 
+scene.add(cube2);
+
+const cube3 = createCubeObstacle({ x: 0, y: 1, z: -20 }, { x: 7, y: 2, z: 7 }); 
+scene.add(cube3);
 
 let mixers = [];
 
